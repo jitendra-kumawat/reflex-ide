6e1e7db350274b08b4bd29a7a79ee7a14e111928
Jitendra Kumawat <Jitendra.Kumawat@guavus.com>
2018-10-10 12:35:57 +0530

    update

--- routes/getUsersCount.js
+++ routes/getUsersCount.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET getUsersCount listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(JSON.stringify({
+    "count": 2020202
+  }))
 });
 
 module.exports = router;
